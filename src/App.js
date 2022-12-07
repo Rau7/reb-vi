05ba93f01309d002c9e0e4bbbc0488a8bffb3cfd
Alp Toker <tokeralp@gmail.com>
2022-12-08 02:11:16 +0300

    navbarMobile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import "./css/stylev3.css";
 import "./css/mobile.scss";
+import "./css/mobilesmwidth.scss";
+import "./css/mobile500height.scss";
+import "./css/mobilexlwidth.scss";
 import Landingv2 from "./pages/Landingv2";
 import Contact from "./pages/Contact";
 
